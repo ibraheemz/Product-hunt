84cca3618d080eb87b90f72b4a7ec1b6fdaf8456
ibrahim rwash <ibrahemrwash18@gmail.com>
2021-02-22 00:01:46 +0200

    modified:   src/SignUpModal.js

--- src/SignUpModal.js
+++ src/SignUpModal.js
@@ -1,5 +1,6 @@
 import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from './kitty.webp'
 function LoginModal(props) {
     return (
         <Modal
@@ -8,22 +9,58 @@ function LoginModal(props) {
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Modal heading
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <h1>SIGN UP here</h1>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo
-                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                    risus, porta ac consectetur ac, vestibulum at eros.
-                </p>
+            <Modal.Body className="text-center ml-4 mr-4">
+                <img
+                    className="login-image"
+                    src={logo}
+                    alt="login kitty logo"
+                ></img>
+                <div className="mt-4">
+                    <h1>SIGN UP here</h1>
+                    <h4>Centered Modal</h4>
+                    <p>
+                        Cras mattis consectetur purus sit amet fermentum. Cras
+                        justo odio, dapibus ac facilisis in, egestas eget quam.
+                        Morbi leo Morbi leo risus, porta ac consectetur ac,
+                        vestibulum at eros.
+                    </p>
+                    <div className="d-flex justify-content-center justify-content-between">
+                        <a
+                            class="btn btn-primary btn-sm m-1 "
+                            href="#"
+                            role="button"
+                        >
+                            LOG IN WITH TWITTER
+                        </a>
+                        <a
+                            class="btn btn-primary btn-sm m-1 "
+                            href="#"
+                            role="button"
+                        >
+                            LOG IN WITH FACEBOOK
+                        </a>
+                        <a
+                            class="btn btn-primary btn-sm m-1 "
+                            href="#"
+                            role="button"
+                        >
+                            LOG IN WITH GOOGLE
+                        </a>
+                        <a
+                            class="btn btn-primary btn-sm m-1 "
+                            href="#"
+                            role="button"
+                        >
+                            LOG IN WITH ANGELLIST
+                        </a>
+                    </div>
+                </div>
             </Modal.Body>
-            <Modal.Footer>
-                <button onClick={props.onHide}>Close</button>
+            <Modal.Footer className="d-flex justify-content-center">
+                <p className="login-footer">
+                    We'll never post to any of your accounts without your
+                    permission.
+                </p>
             </Modal.Footer>
         </Modal>
     )
